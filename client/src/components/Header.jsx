dba458aa4880fadede88e047143ce2d886600516
Felype Henrique <felypehenrique25@gmail.com>
2024-01-14 10:54:12 -0300

    change project name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>FH</span>
+                        <span className='text-slate-500'>MG</span>
                         <span className='text-slate-900'>_</span>
                         <span className='text-slate-700'>Imoveis</span>
                     </h1>
