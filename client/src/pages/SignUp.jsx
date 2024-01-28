5046ceff5e8da20d4a778dff1d6533c71cc73f1b
Felype Henrique <felypehenrique25@gmail.com>
2024-01-28 09:50:07 -0300

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,9 +43,6 @@ export default function SignUp() {
         }
 
     }
-
-    console.log(formData);
-
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl text-center font-semibold my-7'>Cadastrar</h1>
